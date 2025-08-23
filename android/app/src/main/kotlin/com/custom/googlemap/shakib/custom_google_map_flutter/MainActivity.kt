b5b1864fc46e0504c9aa745f367491a25c1e08b1
golamshakib <golamshakib.h@gmail.com>
2025-08-23 12:43:47 +0600

    project setup

---
+++ android/app/src/main/kotlin/com/custom/googlemap/shakib/custom_google_map_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.custom.googlemap.shakib.custom_google_map_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'app.dart';
+import 'core/services/auth_service.dart';
+import 'core/utils/logging/loggerformain.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  await AuthService.init();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+        (value) {
+      Logger.init(kReleaseMode ? LogMode.live : LogMode.debug);
+      runApp(const MyApp());
+    },
+  );
+}
