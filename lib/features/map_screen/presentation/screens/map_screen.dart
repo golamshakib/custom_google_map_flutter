200ec78061403b4e708d86bc192ae21068c42671
golamshakib <golamshakib.h@gmail.com>
2025-08-27 15:35:28 +0600

    changes statefull to stateless widget at the home screen

--- lib/features/map_screen/presentation/screens/map_screen.dart
+++ lib/features/map_screen/presentation/screens/map_screen.dart
@@ -5,19 +5,13 @@ import '../../../../core/common/widgets/custom_text.dart';
 import '../../../../core/utils/constants/app_sizes.dart';
 import '../../../authentication/controllers/login_controller.dart';
 
-class MapScreen extends StatefulWidget {
+class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
-
-  @override
-  State<MapScreen> createState() => _MapScreenState();
-}
-
-class _MapScreenState extends State<MapScreen> {
-  LatLng myCurrentLocation = const LatLng(28.3949, 84.1240);
-
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(LoginController());
+    LatLng myCurrentLocation = const LatLng(28.3949, 84.1240);
+
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
